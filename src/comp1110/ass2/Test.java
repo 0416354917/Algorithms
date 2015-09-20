b97c43cf94decf7fac730f8b21c52031efa80414
Jun Chen <tlnd.sky@gmail.com>
2015-09-20 17:18:31 +1000

    done

--- src/comp1110/ass2/Test.java
+++ src/comp1110/ass2/Test.java
@@ -1,41 +1,61 @@
 package comp1110.ass2;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-/**
- * Created by SIQI on 24/08/15.
- */
 public class Test {
-    public static void main(String args[])
-    {
-        System.out.println("pleast input a number:");
-        Scanner reader=new Scanner(System.in);
-        String inpt;
-        inpt=reader.next();
-        int inptLength = inpt.length();
-        int strLength;
-        boolean b = true;
-        if(inptLength%3 == 0)
-        {
-            strLength = inptLength/3;
-        }
-        else
-        {
-            b = false;
-            strLength = (int)inptLength/3 + 1;
-        }
-        String[] str = new String[strLength];
-        for(int i=0; i<str.length;i++)
-        {
-            if(b||(i!=str.length-1))
-                str[i] = inpt.substring(3*i,3*i+3);
-            else
-                str[i] = inpt.substring(3*i);
-        }
-        for(int i=0;i<str.length;i++)
-        {
-            System.out.print(str[i] + "   ");
-        }
-        System.out.println();
-    }
+
+	static String pathname = "/Users/Jason/git/Algorithms/src/comp1110/ass2/data.txt";
+	static HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+
+	static String outputFilePath = "/Users/Jason/git/Algorithms/src/comp1110/ass2/output.txt";
+
+	/**
+	 * Parse data.txt.
+	 * 
+	 * @param pathname
+	 * @throws Exception
+	 */
+	public static void parseGameState(String pathname) throws Exception {
+		File in = new File(pathname);
+		BufferedReader br = new BufferedReader(new FileReader(in));
+
+		File out = new File(outputFilePath);
+		BufferedWriter bw = new BufferedWriter(new FileWriter(out));
+
+		String line = br.readLine();
+		while (line != null) {
+			StringBuffer sb = new StringBuffer();
+			sb.append("list = new ArrayList<>(); \n");
+
+			// parse a single line:
+			String key = line.split(":")[0].trim();
+			String[] valueStr = line.split(":")[1].trim().split(",");
+			ArrayList<Integer> valueList = new ArrayList<Integer>();
+			for (String val : valueStr) {
+				// valueList.add(Integer.parseInt(val.trim()));
+				sb.append("list.add(" + val.trim() + "); \n");
+			}
+
+			sb.append("map.put(" + key + ", " + "list); \n");
+			bw.write(sb.toString());
+
+			line = br.readLine();
+		}
+
+		bw.close();
+		br.close();
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		parseGameState(pathname);
+	}
+
 }
