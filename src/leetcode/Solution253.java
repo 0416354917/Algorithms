14d808271472520ef869db8f9e09324b398aac62
Jun Chen <tlnd.sky@gmail.com>
2016-06-07 22:04:03 +1000

    #253. Meeting Rooms II

--- src/leetcode/Solution253.java
+++ src/leetcode/Solution253.java
@@ -5,6 +5,39 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Meeting Rooms II.
+ * 
+ * <p>
+ * <b>Description: </b>Given an array of meeting time intervals consisting of
+ * start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number
+ * of conference rooms required.
+ * 
+ * For example, Given [[0, 30],[5, 10],[15, 20]], return 2.
+ * 
+ * Show Company Tags Show Tags Show Similar Problems
+ * 
+ * </p>
+ * 
+ * <p>
+ * <b>Algorithm: </b>Greedy.
+ * </p>
+ * 
+ * <p>
+ * <b>Reference: </b>...
+ * </p>
+ * 
+ * <p>
+ * <b>Note: </b>...
+ * <p>
+ * 
+ * <p>
+ * <b>Status: </b>Accepted.
+ * </p>
+ * 
+ * @author Jason
+ *
+ */
 public class Solution253 {
 
 	// Definition for an interval.
@@ -50,16 +83,28 @@ public class Solution253 {
 			}
 
 		});
-		List<Integer> end = new ArrayList<>();
-		end.add(intervals[0].end);
+
+		List<List<Interval>> rooms = new ArrayList<>();
+
+		List<Interval> room1 = new ArrayList<>();
+		room1.add(intervals[0]);
+		rooms.add(room1);
 		ans++;
-		int i = 0;
-		int minStart = intervals[0].end;
-		while (i < intervals.length - 1) {
-			i++;
+		for (int i = 1; i < intervals.length; i++) {
 			Interval it = intervals[i];
-			if(it.end < minStart) {
-				
+			boolean scheduled = false;
+			for (List<Interval> room : rooms) {
+				if (room.get(room.size() - 1).end <= it.start) {
+					room.add(it);
+					scheduled = true;
+					break;
+				}
+			}
+			if (!scheduled) {
+				List<Interval> newRoom = new ArrayList<>();
+				newRoom.add(it);
+				rooms.add(newRoom);
+				ans++;
 			}
 		}
 
@@ -67,8 +112,14 @@ public class Solution253 {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Solution253 solution = new Solution253();
 
+		Interval it1 = solution.new Interval(0, 30);
+		Interval it2 = solution.new Interval(5, 10);
+		Interval it3 = solution.new Interval(15, 20);
+
+		Interval[] intervals = { it1, it2, it3 };
+		System.out.println(solution.minMeetingRooms(intervals));
 	}
 
 }
