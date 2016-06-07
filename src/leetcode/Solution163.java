83a888e22823c3fd0cc539632a11fb7034db4a9f
Jun Chen <tlnd.sky@gmail.com>
2016-06-07 22:59:06 +1000

    #259. 3Sum Smaller

--- src/leetcode/Solution163.java
+++ src/leetcode/Solution163.java
@@ -25,28 +25,56 @@ public class Solution163 {
 			}
 		}
 
+		if (i == nums.length) {
+			if (lower == upper)
+				ans.add("" + lower);
+			else
+				ans.add(lower + "->" + upper);
+			return ans;
+		}
+
 		// upper:
 		int j = i;
-		for (; j < nums.length; j++) {
-			if (upper > nums[j]) {
-				break;
-			}
+		while (j < nums.length && upper > nums[j]) {
+			j++;
 		}
-		
 
-		// if (i == nums.length) {
-		// if (lower == upper)
-		// ans.add("" + lower);
-		// else
-		// ans.add(lower + "->" + upper);
-		// return ans;
-		// }
-		//
-		// if (lower == nums[i] - 2) {
-		// ans.add("" + (lower + 1));
-		// } else if (lower < nums[i] - 2) {
-		// ans.add((lower + 1) + "->" + (nums[i] - 1));
-		// }
+		if (j == i) {
+			if (lower == upper) {
+				if (nums[i] != lower)
+					ans.add("" + lower);
+			} else {
+				if (upper != nums[i])
+					ans.add(lower + "->" + upper);
+				else {
+					if (lower == upper - 1) {
+						ans.add("" + lower);
+					} else {
+						ans.add(lower + "->" + (upper - 1));
+					}
+				}
+			}
+			return ans;
+		}
+
+		int last = lower;
+		int k = i;
+		for (; k <= j - 1; k++) {
+			if (last + 1 == nums[k]) {
+				ans.add("" + (last));
+			} else if (last + 1 < nums[k]) {
+				ans.add((last) + "->" + (nums[k] - 1));
+			}
+			last = nums[k];
+		}
+
+		k--;
+		if (nums[k] + 1 == upper) {
+			ans.add("" + upper);
+		}
+		if (nums[k] + 1 < upper) {
+			ans.add((nums[k] + 1) + "->" + (upper));
+		}
 
 		return ans;
 	}
@@ -54,9 +82,12 @@ public class Solution163 {
 	public static void main(String[] args) {
 		Solution163 solution = new Solution163();
 
-		int[] nums = { 0, 1, 3, 50, 75 };
-		int lower = 0;
-		int upper = 99;
+		// int[] nums = { 0, 1, 3, 50, 75 };
+		// int lower = 0;
+		// int upper = 99;
+		int[] nums = { -1 };
+		int lower = -2;
+		int upper = 0;
 
 		List<String> ans = solution.findMissingRanges(nums, lower, upper);
 		ans.stream().forEach(x -> System.out.println(x));
